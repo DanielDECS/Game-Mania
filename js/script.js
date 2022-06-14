0dacfd867c80df7fb0d530911541a577414b94e3
Daniel <dancarsoa@gmail.com>
2022-06-14 12:12:43 -0400

    Iniciada a configuração da estrutura de menu carousel

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
+// Função de exibição do menu de barras para layout responsivo
 $(document).ready(function() {
     $("#bars").click(function() {
-
-        if($("#menu").hasClass("menu-active")) {
-            $("#menu").removeClass("menu-active");
-        } else {
-            $("#menu").addClass("menu-active");
-        }
+        // Menu carousel
+        let listCard = [product1, product2, product3, product4]
+        
+        
+        
+        
+        
+        
+        // Toggle Jquery
+        $("#menu").toggleClass("menu-active")
+        
+        // Toggle manual
+        // if($("#menu").hasClass("menu-active")) {
+        //     $("#menu").removeClass("menu-active");
+        // } else {
+        //     $("#menu").addClass("menu-active");
+        // }
     })
 })
 
+// function showMenu() {
+//     let menu = document.getElementById("menu");
 
+//     if(getComputedStyle(menu).display == "none") {
+//         menu.style.display = "flex";
+//     } else {
+//         menu.style.display = "none"
+//     }
+// }
 
-function showMenu() {
-    let menu = document.getElementById("menu");
-
-    if(getComputedStyle(menu).display == "none") {
-        menu.style.display = "flex";
-    } else {
-        menu.style.display = "none"
-    }
-}
-
+// Função teste para registro de usuário
 function registerUser() {
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
